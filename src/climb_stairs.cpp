67a7f2f4208e78177d147f53119da654e41d01ad
Hizamer <848863325@qq.com>
2025-10-27 21:33:12 +0800

    climb stairs: dp

--- src/climb_stairs.cpp
+++ src/climb_stairs.cpp
@@ -4,32 +4,42 @@
 class Solution {
 public:
     int climbStairs(int n) { 
-        std::vector<int> memo(n+1, 0);
+        std::vector<int> dp(n+1, 0);
+        dp[0] = dp[1] = 1;
 
-        return recursion_process(n, memo);
-    }
-
-    int recursion_process(int n, std::vector<int>& memo) {
-        if(memo[n]) return memo[n]; 
-        if(n == 0 || n == 1) {
-            return 1;
-            memo[n] = 1;
+        for(int i = 2; i < dp.size(); ++i) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        
-        if(!memo[n-1]) memo[n-1] = recursion_process(n-1, memo);
-        if(!memo[n-2]) memo[n-2] = recursion_process(n-2, memo);
-         
-        memo[n] = memo[n-1] + memo[n-2];
-        
-        return memo[n];
+
+        return dp[n];
     }
+    // int climbStairs(int n) { 
+    //     std::vector<int> memo(n+1, 0);
+
+    //     return recursion_process(n, memo);
+    // }
+
+    // int recursion_process(int n, std::vector<int>& memo) {
+    //     if(memo[n]) return memo[n]; 
+    //     if(n == 0 || n == 1) {
+    //         return 1;
+    //         memo[n] = 1;
+    //     }
+        
+    //     if(!memo[n-1]) memo[n-1] = recursion_process(n-1, memo);
+    //     if(!memo[n-2]) memo[n-2] = recursion_process(n-2, memo);
+         
+    //     memo[n] = memo[n-1] + memo[n-2];
+        
+    //     return memo[n];
+    // }
 };
 
 int main(int argc, char const* argv[])
 {
     Solution sol;
 
-    int res = sol.climbStairs(6);
+    int res = sol.climbStairs(3);
 
     std::cout << res << std::endl;
 
