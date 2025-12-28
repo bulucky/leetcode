4070281f7dd38dd441fc4e9aaa2d7266bd94c095
Hizamer <848863325@qq.com>
2025-12-28 21:10:09 +0800

    ⚡️ liudahu

--- src/41_missing_first_positive_number.cpp
+++ src/41_missing_first_positive_number.cpp
@@ -1,36 +1,53 @@
 #include <vector>
 #include <iostream>
-#include <unordered_map>
 
 class Solution {
 public:
-    // 结果只能在1~nums.size()
-    // 存储nums所有正整数,从1开始遍历存储正整数,若存在则结果只能是存在数+1
+    // liudahu
     int firstMissingPositive(std::vector<int>& nums) {
-        std::unordered_map<int, int> positive_map;
-        int res = 1;
-        for (const auto& num : nums) {
-            if (num > 0 && num <= nums.size()) {
-                positive_map[num] = num;
+        int res = nums.size();
+        for (int i = 0; i < nums.size(); ++i) {
+            while (nums[i] >= 1 && nums[i] <= nums.size() && nums[i] != nums[nums[i] - 1]) {
+                std::swap(nums[i], nums[nums[i] - 1]);
             }
         }
 
-        for (int i = 0; i < positive_map.size(); ++i) {
-            if (positive_map.find(res) != positive_map.end()) {
-                ++res;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
             }
         }
 
-        return res;
+        return res + 1;
     }
 };
 
 int main(int atgc, char const* argv[]) {
     Solution sol;
 
-    std::vector<int> nums = {7, 8, 9, 11, 12};
+    std::vector<int> nums = {-2147483648};
     int res = sol.firstMissingPositive(nums);
     std::cout << res << "\n";
 
     return 0;
 }
+
+// // 结果只能在1~nums.size()
+// // 存储nums所有正整数,从1开始遍历存储正整数,若存在则结果只能是存在数+1
+// int firstMissingPositive(std::vector<int>& nums) {
+//     std::unordered_map<int, int> positive_map;
+//     int res = 1;
+//     for (const auto& num : nums) {
+//         if (num > 0 && num <= nums.size()) {
+//             positive_map[num] = num;
+//         }
+//     }
+
+//     for (int i = 0; i < positive_map.size(); ++i) {
+//         if (positive_map.find(res) != positive_map.end()) {
+//             ++res;
+//         }
+//     }
+
+//     return res;
+// }
