53f193dff750a091f0be72c00131826017861a8c
Hizamer <848863325@qq.com>
2026-01-31 23:31:05 +0800

    739 monotonic stack

--- src/739_daily_temperatures.cpp
+++ src/739_daily_temperatures.cpp
@@ -5,21 +5,16 @@
 class Solution {
 public:
     std::vector<int> dailyTemperatures(std::vector<int>& temperatures) {
-        std::vector<int> res;
+        std::vector<int> res(temperatures.size(), 0);
         std::stack<int> stack_i;
-        stack_i.push(-1);
-        for (int i = 0; i < temperatures.size(); ++i) {
-            int j = i + 1;
-            while (j < temperatures.size() && temperatures[i] >= temperatures[j]) {
-                ++j;
-            }
-            // 直到结束未找到更高温度
-            if (j == temperatures.size()) {
-                res.push_back(0);
-            } else {
-                res.push_back(j - stack_i.top() - 1);
-            }
 
+        for (int i = temperatures.size() - 1; i >= 0; --i) {
+            while (!stack_i.empty() && temperatures[i] >= temperatures[stack_i.top()]) {
+                stack_i.pop();
+            }
+            if (!stack_i.empty()) {
+                res[i] = stack_i.top() - i;
+            }
             stack_i.push(i);
         }
 
@@ -33,7 +28,7 @@ int main(int argc, char const* argv[]) {
     // std::vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
     // std::vector<int> temperatures = {30,40,50,60};
     // std::vector<int> temperatures = {30,60,90};
-    std::vector<int> temperatures = {89,62,70,58,47,47,46,76,100,70};
+    std::vector<int> temperatures = {89, 62, 70, 58, 47, 47, 46, 76, 100, 70};
 
     std::vector<int> res = sol.dailyTemperatures(temperatures);
     for (const auto& a_res : res) {
@@ -42,3 +37,25 @@ int main(int argc, char const* argv[]) {
 
     return 0;
 }
+
+// std::vector<int> dailyTemperatures(std::vector<int>& temperatures) {
+//     std::vector<int> res;
+//     std::stack<int> stack_i;
+//     stack_i.push(-1);
+//     for (int i = 0; i < temperatures.size(); ++i) {
+//         int j = i + 1;
+//         while (j < temperatures.size() && temperatures[i] >= temperatures[j]) {
+//             ++j;
+//         }
+//         // 直到结束未找到更高温度
+//         if (j == temperatures.size()) {
+//             res.push_back(0);
+//         } else {
+//             res.push_back(j - stack_i.top() - 1);
+//         }
+
+//         stack_i.push(i);
+//     }
+
+//     return res;
+// }
