d7059836d40cbb9c6e54a0f1de0c15ce6a4ed8af
hizam <848863325@qq.com>
2026-02-28 23:46:24 +0800

    optimize

--- src/74_search_matrix.cpp
+++ src/74_search_matrix.cpp
@@ -26,6 +26,7 @@ public:
             }
         }
 
+
         return false;
     }
 };
@@ -35,7 +36,7 @@ int main(int argc, char const* argv[]) {
     std::vector<std::vector<int>> matrix = {{1, 3, 5, 7},
                                             {10, 11, 16, 20},
                                             {23, 30, 34, 60}};
-    
+
     bool res = sol.searchMatrix(matrix, 3);
     std::cout << res << "\n";
 
